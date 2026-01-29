07fa7191fc329bf725d508c716d38746dff0a770
Samin Ahmed <saminahmed1001@gmail.com>
2026-01-29 21:10:40 +0600

    Feat: add header, contentDiv and footer

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header/Header";
+
+export default function App() {
+  return (
+    <div className="appDiv">
+      <Header></Header>
+      <div className="contentDiv"></div>
+      <footer></footer>
+    </div>
+  );
+}
