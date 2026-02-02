bfdb4b655c3527e5c98216435977227e7c56399b
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-03 01:44:23 +0600

    Refactor: change cart button name to headerAct button

--- src/Components/Header/Navbar/Nav.jsx
+++ src/Components/Header/Navbar/Nav.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router";
+import { useLocation } from "react-router-dom";
 
-import CartBtn from "./CartBtn/CartBtn.jsx";
+import HeaderActBtn from "./HeaderActBtn/HeaderActBtn.jsx";
 import styles from "./Nav.module.css";
 
-export default function Nav({ count }) {
+export default function Nav({ cartItems }) {
+  const { pathname } = useLocation();
+  const isShop = pathname.startsWith("/shop");
+
   return (
     <nav className={styles.nav}>
-      <Link className={styles.cartLink} to="cart">
-        <CartBtn count={count} />
+      <Link className={styles.HeaderActBtnLink} to={isShop ? "cart" : "shop"}>
+        <HeaderActBtn cartItems={cartItems} isShop={isShop} />
       </Link>
     </nav>
   );
