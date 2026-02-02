bf3cc5996dcfee6a28a9e707211edb8725b1b61b
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-03 01:48:17 +0600

    add header Act button for easy navigation

---
+++ src/Components/Header/Navbar/HeaderActBtn/HeaderActBtn.jsx
@@ -0,0 +1,46 @@
+import styles from "./HeaderActBtn.module.css";
+
+export default function HeaderActBtn({ cartItems, isShop }) {
+  const getCount = () => {
+    let count = 0;
+    Object.values(cartItems).map((itemObj) => {
+      count += itemObj.quantity;
+    });
+
+    return count;
+  };
+  const count = getCount();
+
+  const getBtn = () => {
+    if (isShop) {
+      return (
+        <button className={styles.actButton}>
+          <div className={styles.cartBtnTitle}>
+            <img src="/src/assets/icons/cart.svg" alt="Cart Icon" />
+            <p>Cart</p>
+          </div>
+          {count > 0 && (
+            <div className={styles.countDiv}>
+              <div className={styles.cartCount}>{count}</div>
+            </div>
+          )}
+        </button>
+      );
+    } else {
+      return (
+        <button className={styles.actButton}>
+          <div className={styles.cartBtnTitle}>
+            <img
+              className={styles.storeIcon}
+              src="/src/assets/icons/store.svg"
+              alt="Cart Icon"
+            />
+            <p>Store</p>
+          </div>
+        </button>
+      );
+    }
+  };
+
+  return <div className="cartDiv">{getBtn()}</div>;
+}
