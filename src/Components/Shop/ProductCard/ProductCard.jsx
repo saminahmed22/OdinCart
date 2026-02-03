a22e8afde7aa917ace0b1f18afe09912697c6de0
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-03 17:42:01 +0600

    Refactor: move quantity field to dedicated file

--- src/Components/Shop/ProductCard/ProductCard.jsx
+++ src/Components/Shop/ProductCard/ProductCard.jsx
@@ -1,25 +1,9 @@
-import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import styles from "./ProductCard.module.css";
+import QuantityField from "../../QuantityField/QuantityField";
 
 function CartButton({ id, title, price, image }) {
   const [cartItems, setCartItems] = useOutletContext();
-  const [cartQuantity, setQuantity] = useState(
-    cartItems[id]?.quantity > 0 ? cartItems[id].quantity : 1,
-  );
-
-  function handleQuantityChange(e, manualQuantity = null) {
-    manualQuantity ? setQuantity(manualQuantity) : setQuantity(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const inputFieldValue = e.target.elements.quantityField.value;
-    setCartItems((prev) => ({
-      ...prev,
-      [id]: { ...prev[id], quantity: Number(inputFieldValue) },
-    }));
-  }
 
   if (!cartItems[id]?.quantity) {
     return (
@@ -41,56 +25,7 @@ function CartButton({ id, title, price, image }) {
       </button>
     );
   } else {
-    return (
-      <div className={styles.clickedCartButtonDiv}>
-        <button
-          className={styles.quantityAddBtn}
-          onClick={(e) => {
-            const currentCount = cartItems[id].quantity + 1;
-            handleQuantityChange(e, currentCount);
-            setCartItems((prev) => ({
-              ...prev,
-              [id]: { ...prev[id], quantity: currentCount },
-            }));
-          }}
-          disabled={cartItems[id].quantity >= 50}
-        >
-          <img
-            className={styles.quantityAddIcon}
-            src="/assets/icons/plus.svg"
-            alt=""
-          />
-        </button>
-        <form onSubmit={handleSubmit}>
-          <input
-            name="quantityField"
-            className={styles.cartQuantity}
-            type="number"
-            min={0}
-            max={50}
-            value={cartQuantity}
-            onChange={handleQuantityChange}
-          />
-        </form>
-        <button
-          className={styles.quantityRemoveBtn}
-          onClick={(e) => {
-            const currentCount = cartItems[id].quantity - 1;
-            handleQuantityChange(e, currentCount);
-            setCartItems((prev) => ({
-              ...prev,
-              [id]: { ...prev[id], quantity: currentCount },
-            }));
-          }}
-        >
-          <img
-            className={styles.quantityRemoveIcon}
-            src="/assets/icons/minus.svg"
-            alt=""
-          />
-        </button>
-      </div>
-    );
+    return <QuantityField id={id}></QuantityField>;
   }
 }
 
@@ -118,7 +53,6 @@ export default function ProductCard({
               src="/assets/icons/star.svg"
               alt=""
             />
-
             <p>
               {rating}({ratingCount})
             </p>
