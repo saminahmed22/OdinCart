63cf38a7d2bd5978f16aadefda1d3347bc898b1f
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-03 17:40:07 +0600

    Feat: add delete and quantity adjust buttons for each item card

--- src/Components/Cart/CartCard/CartCard.jsx
+++ src/Components/Cart/CartCard/CartCard.jsx
@@ -1,22 +1,43 @@
 import styles from "./CartCard.module.css";
+import { useOutletContext } from "react-router";
+
+import QuantityField from "../../QuantityField/QuantityField";
 
 export default function CartCard({
+  id,
   title = null,
   price = 0,
   quantity = 0,
   image = null,
 }) {
+  const [cartItems, setCartItems] = useOutletContext();
   const total = price * quantity;
   return (
-    <div className={styles.cartCard}>
-      <div className={styles.titleDiv}>
-        <img className={styles.image} src={image} alt="" />
-        <div className={styles.title}>{title}</div>
+    <div className={styles.cartCardWithDelete}>
+      <div className={styles.cartCard}>
+        <div className={styles.titleDiv}>
+          <img className={styles.image} src={image} alt="" />
+          <div className={styles.title}>{title}</div>
+        </div>
+        <div className={styles.priceDiv}>
+          <div className={styles.price}>${price}</div>
+          <div className={styles.quantity}>
+            <QuantityField id={id}></QuantityField>
+          </div>
+          <div className={styles.total}>${total.toFixed(2)}</div>
+        </div>
       </div>
-      <div className={styles.priceDiv}>
-        <div className={styles.price}>${price}</div>
-        <div className={styles.quantity}>{quantity}</div>
-        <div className={styles.total}>${total}</div>
+      <div className={styles.cartCardRemoveBtnDiv}>
+        <button
+          className={styles.cartCardRemoveBtn}
+          onClick={() => {
+            const cloneObj = structuredClone(cartItems);
+            delete cloneObj[id];
+            setCartItems(cloneObj);
+          }}
+        >
+          <img src="/assets/icons/deleteBlack.svg" alt="Remove item button" />
+        </button>
       </div>
     </div>
   );
