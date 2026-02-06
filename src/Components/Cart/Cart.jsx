9c8eea069f04ea81e1e729deb929d0e86771ce1e
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-07 00:04:45 +0600

    Refacor: various changes

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,68 +6,62 @@ import CartCard from "./CartCard/CartCard";
 
 export default function Cart() {
   const [cartItems, setCartItems] = useOutletContext();
-  const getTotalPrice = () => {
-    let total = 0;
-    Object.values(cartItems).map(
-      (itemObj) => (total += itemObj.price * itemObj.quantity),
+
+  const totalPrice = Object.values(cartItems).reduce(
+    (total, itemObj) => total + itemObj.price * itemObj.quantity,
+    0,
+  );
+
+  if (totalPrice <= 0) {
+    return (
+      <div className={`itemListDiv ${styles.cartItemListDiv}`}>
+        <div className={`${styles.cartList} ${styles.cartListEmpty}`}>
+          <p>Cart is empty</p>
+        </div>
+      </div>
     );
-    return total.toFixed(2);
-  };
+  } else {
+    return (
+      <div className={`itemListDiv ${styles.cartItemListDiv}`}>
+        <div className={styles.cartList}>
+          <div className={styles.cartRemoveBtnDiv}>
+            <button
+              title="Clear cart"
+              aria-label="Clear cart"
+              className={styles.cartRemoveBtn}
+              onClick={() => {
+                setCartItems({});
+              }}
+            >
+              <img
+                src="/assets/icons/deleteBlack.svg"
+                alt="Remove all button"
+              />
+              <p>Clear cart</p>
+            </button>
+          </div>
 
-  const getCartContent = () => {
-    if (getTotalPrice() > 0) {
-      return (
-        <div className={`itemListDiv ${styles.cartItemListDiv}`}>
-          <div className={styles.cartList}>
-            <div className={styles.cartRemoveBtnDiv}>
-              <button
-                className={styles.cartRemoveBtn}
-                onClick={() => {
-                  setCartItems({});
-                }}
-              >
-                <img
-                  src="/assets/icons/deleteBlack.svg"
-                  alt="Remove all button"
-                />
-                <p>Remove all</p>
-              </button>
+          {Object.keys(cartItems).map(
+            (id) =>
+              cartItems[id].quantity > 0 && (
+                <CartCard
+                  key={`cart_${cartItems[id].title}`}
+                  id={id}
+                ></CartCard>
+              ),
+          )}
+          <div className={styles.cartListFooter}>
+            <div className={styles.cartListFooterTitle}>
+              <b className={styles.cartListFooterTexts}>Net total</b>
             </div>
-
-            {Object.keys(cartItems).map(
-              (id) =>
-                cartItems[id].quantity > 0 && (
-                  <CartCard
-                    key={`cart_${cartItems[id].title}`}
-                    id={id}
-                    title={cartItems[id].title}
-                    price={cartItems[id].price}
-                    quantity={cartItems[id].quantity}
-                    image={cartItems[id].image}
-                  ></CartCard>
-                ),
-            )}
-            <div className={styles.cartListFooter}>
-              <div className={styles.cartListFooterTitle}>
-                <b className={styles.cartListFooterTexts}>Net Total</b>
-              </div>
-              <div className={styles.cartListFooterPrice}>
-                <b className={styles.cartListFooterTexts}>${getTotalPrice()}</b>
-              </div>
+            <div className={styles.cartListFooterPrice}>
+              <b className={styles.cartListFooterTexts}>
+                ${totalPrice.toFixed(2)}
+              </b>
             </div>
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div className={`itemListDiv ${styles.cartItemListDiv}`}>
-          <div className={`${styles.cartList} ${styles.cartListEmpty}`}>
-            <p>Cart is empty</p>
-          </div>
-        </div>
-      );
-    }
-  };
-
-  return getCartContent();
+      </div>
+    );
+  }
 }
